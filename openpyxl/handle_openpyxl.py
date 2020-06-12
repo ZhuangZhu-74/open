a5314f67f66a69fb76f57065089fd66222cf2b22
ZhuangZhu-74 <49544524+ZhuangZhu-74@users.noreply.github.com>
2020-06-12 17:17:18 +0800

    Update handle_openpyxl.py
    更新封装方法

--- openpyxl/handle_openpyxl.py
+++ openpyxl/handle_openpyxl.py
@@ -20,92 +20,92 @@ import sys, os, shutil
 import openpyxl
 from openpyxl.utils.cell import get_column_letter, column_index_from_string
 
-from openpyxl.worksheet.worksheet import Worksheet
-
 base_path = os.getcwd()
 sys.path.append(base_path)
 
-test_path = r'.'
-excel_file = os.path.join(test_path, 'test.xlsx')
 
-
-def use_backup(newname, old_file=excel_file):
-    '''
-    backup file
-    '''
-    
-    if os.path.isfile(newname) and os.path.getsize(newname):
-        pass
-    else:
-        os.path.isfile(old_file)
-        shutil.copy(old_file, newname)
-        '''
-        global 分开写，不然会报错
-        '''
-        
-    global excel_file
-    excel_file = newname
-    return excel_file
-    
-    
-def load_excel():
-    """
-    load excel file
-    """
-    try:
-        wb = openpyxl.load_workbook(excel_file)
-    except Exception as e:
-        print(excel_file)
-        print(e)
-        return None
-    return wb
-
-        
-def sheet_by_name(wb, name=None):
-    '''
-    same method name like 'xlrd'
-    '''
-    
-    all_sheet_names = wb.sheetnames
-    if name != None:
-        if name in all_sheet_names:
-            ws = wb[name]
-        else:
-            print("No such name {}".format(name))
-            return None
-    else:
-        ws = wb.active
-    return ws
-
-def sheet_by_index(wb, index=None):
-    '''
-    same method name like 'xlrd'
-    index >= 0
-    '''
-    
-    all_sheet_names = wb.sheetnames
-    if index in range(len(all_sheet_names)):
-        ws = wb[all_sheet_names[index]]
-    elif index == None:
-        ws = wb.active
-    else:
-        print("index illegal")
-        return None
-    return ws
-
-
-class test_raw():
-    def __init__(self, wb, ws):
+class handle_excel():
+    def __init__(self, xlsx_file):
         try:
-            if isinstance(ws, Worksheet):
-                self.ws = ws
-            if isinstance(wb, openpyxl.Workbook):
-                self.wb = wb
+            self.file = xlsx_file
+            self.wb = self.load_excel()
+            self.ws = None
+            
         except Exception as e:
             print('init error: ')
             print(e)
+            
+    @staticmethod
+    def use_backup(newname, old_file=None):
+        '''
+        backup file
+        '''
+        if old_file == None:
+            old_file = "c:\test.xlsx"
+        if os.path.isfile(newname) and os.path.getsize(newname):
+            pass
+        else:
+            os.path.isfile(old_file)
+            shutil.copy(old_file, newname)
+            
+        '''
+        global 分开写，不然会报错
+        '''
+        global excel_file
+        excel_file = newname
+        
+        return excel_file
+    
+    
+    def load_excel(self):
+        """
+        load excel file
+        """
+        try:
+            wb = openpyxl.load_workbook(self.file)
+        except Exception as e:
+            print(self.file)
+            print(e)
+            return None
+        return wb
+            
+    def sheet_by_name(self, name=None):
+        '''
+        same method name like 'xlrd'
+        '''
+        
+        all_sheet_names = self.wb.sheetnames
+        if name != None:
+            if name in all_sheet_names:
+                self.ws = self.wb[name]
+            else:
+                print("No such name {}".format(name))
+                return None
+        else:
+            self.ws = self.wb.active
+        return self.ws
+    
+    def sheet_by_index(self, index=None):
+        '''
+        same method name like 'xlrd'
+        index >= 0
+        '''
+        
+        all_sheet_names = self.wb.sheetnames
+        if index in range(len(all_sheet_names)):
+            self.ws = self.wb[all_sheet_names[index]]
+        elif index == None:
+            self.ws = self.wb.active
+        else:
+            print("index illegal")
+            return None
+        return self.ws
 
-    def save_wb(self):
+    def save_wb(self, excel_file=None):
+        if excel_file == None:
+            excel_file = self.file
+        else:
+            excel_file = excel_file
         self.wb.save(excel_file)
         
     @staticmethod
@@ -121,6 +121,14 @@ class test_raw():
             col_index = col
             col_tag = get_column_letter(col)
         return col_index, col_tag
+    
+    @property
+    def howmany_rows(self):
+        return self.ws.max_row - self.ws.min_row + 1
+    
+    @property
+    def howmany_cols(self):
+        return self.ws.max_column - self.ws.min_column + 1
         
         
     def get_all_values_by_rows(self):
@@ -179,7 +187,7 @@ class test_raw():
         if row in range(self.ws.min_row, self.ws.max_row+1):
             if col_index in range(self.ws.min_column, self.ws.max_column+1):
                 return self.ws.cell(row, col_index).value
-    
+
     def write_cell_value(self, row, col, value):
         """
         write a cell. but not save.
@@ -190,7 +198,6 @@ class test_raw():
         self.ws.cell(row, col_index, value)
         #self.wb.save(excel_file)
 
-            
     def write_col_values(self, data, col, row_start=1):
         '''
         data: tuple or list for write, 
@@ -218,20 +225,13 @@ class test_raw():
 
 
 if __name__ == '__main__':
-    use_backup(r'.\test_backup.xlsx')
-    wb = load_excel()
-    ws = sheet_by_index(wb, 1)
-    handle = test_raw(wb, ws)
-    
-    handle.write_cell_value(6, 6, "newtest")
-    print(handle.get_cell_value(row=6, col=6))
-    
-    handle.write_col_values(['a','b','c'], 8, 3)
-    print(handle.get_col_values(8))
-    
-    handle.write_row_values(['a','b','c'], 8, 3)
-    print(handle.get_row_values(8))
-    
-    handle.save_wb()
+    test_path = r'.'
+    excel_file = os.path.join(test_path, 'test.xlsx')
+    handle = handle_excel(excel_file)
+    print(handle.ws)
+    ws2 = handle.sheet_by_index()
+    print(handle.ws)
+    print(ws2)
+    print(handle.get_row_values(1))
     
     
